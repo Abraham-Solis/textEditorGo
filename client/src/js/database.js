68d9b80de0fb156eae7d580e15526adbacec293f
email <abe.solis2015@gmail.com>
2022-01-19 18:29:46 -0800

    code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,27 +14,27 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
+  console.log('Into Data Base');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('data saved to the database', result.value);
+  console.log('Your data has been saved to DB', result.value);
 };
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
+  console.log('Grabbing from DB');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result
-    ? console.log('data retrieved from the database', result.value)
-    : console.log('data not found in the database');
+    ? console.log('Retrieved Data', result.value)
+    : console.log('Data is not found');
   return result?.value;
 };
 
