4f95575978982648c7b35b9cd67a087d72efe6a0
email <abe.solis2015@gmail.com>
2022-01-19 18:27:10 -0800

    code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
+  console.log('data saved to the database', result.value);
 };
 
 
