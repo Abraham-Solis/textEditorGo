3be4e879462c45429d7e68516c54f4e642ee6b12
email <abe.solis2015@gmail.com>
2022-01-19 18:26:53 -0800

    Final Code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -27,4 +27,4 @@ butInstall.addEventListener('click', async () => {
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   window.deferredPrompt = null;
- });
+});
